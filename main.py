d077857d0b768c6c351c060a5ee2fa9579e20963
HaloMaster6482 <tavishsingh710@gmail.com>
2026-07-04 10:38:36 +0200

    Fix indentation aesthetics

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ def name(): return input(f"{Fore.YELLOW}Please enter your{Fore.GREEN} name{Fore.
 def mood(name):
     mood = input(Fore.YELLOW + f"How is your {Fore.GREEN}mood{Fore.YELLOW} today? {Fore.BLUE}").lower()
 
-    if "good" in mood:
-        print(Fore.YELLOW + f"Wish you a happy day ahead, {name}! 😊")
-    elif "bad" in mood:
-        print(Fore.YELLOW + f"Hope your day gets better soon, {name}!")
-    else:
-        print(Fore.YELLOW + f"I understand, {name}.")
+    if "good" in mood: print(Fore.YELLOW + f"Wish you a happy day ahead, {name}! 😊")
+    elif "bad" in mood: print(Fore.YELLOW + f"Hope your day gets better soon, {name}!")
+    else: print(Fore.YELLOW + f"I understand, {name}.")
 
 def hobby():
     hobby = input(Fore.YELLOW + f"What is {Fore.GREEN}a favorite hobby{Fore.YELLOW} of yours?{Fore.BLUE} ").upper()
@@ -21,13 +18,9 @@ def hobby():
 def chat_again():
     while True:
         chat_again = input(Fore.YELLOW + f"Do you want to chat again? ({Fore.GREEN}yes{Fore.YELLOW}/{Fore.RED}no{Fore.YELLOW}) " + Fore.BLUE).lower().strip()
-        if chat_again == "yes":
-            return True
-        elif chat_again == "no":
-            return False 
-        else:
-            print(Fore.RED + f"Invalid response. {Fore.YELLOW}Please enter one of the responses shown in the question above.")   
-
+        if chat_again == "yes": return True
+        elif chat_again == "no": return False 
+        else: print(Fore.RED + f"Invalid response. {Fore.YELLOW}Please enter one of the responses shown in the question above.")   
 
 def main():
     while True:
@@ -36,14 +29,9 @@ def main():
         mood(user_name)
         hobby()
 
-        if chat_again() == False:
-            break
+        if chat_again() == False: break
+
     print(Fore.YELLOW + "Thank you for chatting with me!")
     print(f"{Fore.RED}B{Fore.GREEN}y{Fore.BLUE}e{Fore.YELLOW}!")
 
-
-
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": main()
